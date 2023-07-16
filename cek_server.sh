02bf7cc37aab24f224ba85a415db781c9ab01f14
yusufkarbackk <yusufkarback64@gmail.com>
2023-07-16 20:49:20 +0700

    coba konek ke mysql

--- cek_server.sh
+++ cek_server.sh
@@ -1,3 +1,15 @@
+host="localhost"
+port="8889"
+username="root"
+password="root"
+database="bank_sampah"
+
+insertLog(){
+	sql="INSERT INTO data_log (message, down_time, created_at) VALUES ('$1', $2, $3);"
+	mysql -h "$host" -P "$port" -u "$username" -p"$password" "$database" -e "$sql"
+}
+
+
 url="https://bac3-111-94-121-227.ngrok-free.app/"  # Ganti dengan URL server yang ingin Anda pe
 IS_TIME_STARTED="false"
 start_time=0
@@ -14,6 +26,8 @@ while true; do
 			IS_TIME_STARTED="false"
 			unset START_TIME
 			echo "server was down for $duration seconds"
+			insertLog "server was down for $duration seconds" $duration $(date +'%Y-%m-%d %H:%M:%S')
+
 		fi
 	else
     		echo "Server is not responding with status code: $http_code"
